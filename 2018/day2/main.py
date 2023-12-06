69630cabd35ed6a7e4a9d468d840ab7897286548
taholme <theodor@fjellstua.net>
2023-12-06 02:49:51 +0100

    Remaining day2s, day 4 & 5 2023

---
+++ 2018/day2/main.py
@@ -0,0 +1,6 @@
+from collections import Counter
+from itertools import combinations
+lines = open(0).read().strip().splitlines()
+
+print((lambda a,b: a*b)(*[sum(1 for l in lines if x in Counter(l).values()) for x in (2,3)]))
+print((lambda a,b: "".join([a[i] for i in range(len(a)) if a[i] == b[i]]))(*[l for l in combinations(lines, 2) if sum(1 for i in range(len(l[0])-1) if l[0][i] != l[1][i]) < 2][0]))
---
+++ 2020/day2/main.py
@@ -0,0 +1,8 @@
+import re
+lines = open(0).read().strip().splitlines()
+
+
+print(sum(1 for line in lines for s, e, l, p in re.findall(r"(\d+)-(\d+) (\w): (\w+)", line) if p.count(l) in range(int(s), int(e)+1)))
+# print(sum(1 for line in lines for start, end, letter, password in re.findall(r"(\d+)-(\d+) (\w): (\w+)", line) if password.count(letter) in range(int(start), int(end)+1)))
+
+print(sum(1 for line in lines for i1, i2, letter, password in re.findall(r"(\d+)-(\d+) (\w): (\w+)", line) if password[int(i1)] == letter or password[int(i2)] == letter))
